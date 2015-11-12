4ea5fb992e0ad9547b3a1b9bcccc196140b8aa5a
Mike Henry <chsmike@gmail.com>
2015-11-12 12:46:52 -0500

    Initial commit; includes Pod files for Estimote, Parse, and ParseUI; added bridging header; turned on Background Location monitoring

---
+++ AttendanceLog/AttendanceLog/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AttendanceLog
+//
+//  Created by Mike Henry on 11/12/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
